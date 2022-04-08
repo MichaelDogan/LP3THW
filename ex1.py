7ad02810b56f2d8080ecf8fcbb313e11c59fa561
Michael Dogan <MichaelDogan@gmail.com>
2022-04-08 00:23:34 -0400

    add ex1.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# Very first python program example from LP3THW
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do nt touch this.')
