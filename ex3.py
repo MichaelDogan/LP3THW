d50d67ba0d9723f44dab58e8593346ee89e03419
Michael Dogan <MichaelDogan@gmail.com>
2022-04-09 11:12:01 -0400

    complete ex3.py

---
+++ ex3.py
@@ -0,0 +1,17 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#     3 + 2 + 1 - 5 +   0   -   .25 + 6
+#          1 + 0 - .25 + 6
+#               .75 + 6
+#                  6.75
+
+print("Is it true that 3+2 < 5-7?")
+#                       5  !< -2
+
+print(3 + 2 < 5 - 7)
